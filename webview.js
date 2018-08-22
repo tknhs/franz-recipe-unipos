74a1a034fada9d6233849d78a59a0a67c18ea793
tknhs <kdsnlv0109@gmail.com>
2018-08-22 01:33:01 +0000

    set badge: unread notification count

--- webview.js
+++ webview.js
@@ -1,1 +1,8 @@
-module.exports = Franz => Franz;
+module.exports = (Franz) => {
+  const getMessages = () => {
+    const unreadNotifications = document.querySelectorAll('.headerNotify_item.is-unread');
+    Franz.setBadge(unreadNotifications.length);
+  }
+
+  Franz.loop(getMessages);
+}
